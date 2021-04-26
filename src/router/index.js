329235d673b38305a6493100a18fe6224e880036
hjf <625598479@qq.com>
2021-04-26 16:18:50 +0800

    remove baidu

--- src/router/index.js
+++ src/router/index.js
@@ -6,17 +6,11 @@ import LanguageView from './LanguageView.vue'
 
 Vue.use(Router)
 
-const language = localStorage.getItem('language') || (navigator.language == 'zh-CN' ? 'zh-CN' : 'en-US')
 
 export default new Router({
   routes: [
     {
       path: '/',
-      redirect: to => ({name: 'index', params: {lang: language}})
-    },
-    {
-      path: '/:lang',
-      // name: 'lang',
       component: LanguageView,
       children: [
         {
