b12979d9af604a154a505bd358663e01e80b2c94
hjf <625598479@qq.com>
2021-04-27 10:52:51 +0800

    change

--- vue.config.js
+++ vue.config.js
@@ -24,9 +24,5 @@ module.exports = {
     if (process.env.NODE_ENV !== 'development') {
       config.plugins = [...config.plugins, ...plugins]
     }
-    config.externals = {
-      // key：vue是项目里引入时候要用的，value：是开发依赖库的主人定义的不能修改
-      'element-ui': 'ELEMENT'
-    }
   }
 }
